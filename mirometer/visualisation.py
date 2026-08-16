055b1a200c5b2593aebb4bb5956d4ccd106b2af1
DamienGilliard <damien.gilliard@epfl.ch>
2026-08-16 21:47:12 +0200

    feat: add method for plotting of evaluation results

--- mirometer/visualisation.py
+++ mirometer/visualisation.py
@@ -1,4 +1,7 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 def add_text_on_mask(image, mask, text):
     """
@@ -43,3 +46,46 @@ def visualize_image(image):
     cv2.imshow("Image Visualization", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def plot_evaluation(area_ratios):
+    """
+    Plots the evaluation results of area ratios.
+
+    Args:
+        area_ratios (list): List of area ratios to be plotted.
+    """
+    mean = np.mean(area_ratios)
+    std = np.std(area_ratios)
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    plt.title('Area Ratio Distribution on the test set', fontsize=16)
+
+    counts, bins, _ = ax1.hist(area_ratios, bins=30, density=True, alpha=0.6, color='g', label='Density')
+    x = np.linspace(mean - 4*std, mean + 4*std, 1000)
+
+    ax1.set_xlabel('Area Ratio')
+    ax1.set_ylabel('Probability Density', color='g')
+    ax1.tick_params(axis='y', labelcolor='g')
+    ax1.grid(True)
+
+    ax1.text(0.95, 0.95, f'Mean: {mean:.4f}\nStd Dev: {std:.4f}',
+            horizontalalignment='right', verticalalignment='top',
+            transform=ax1.transAxes, bbox=dict(facecolor='white', alpha=0.5))
+
+    ax2 = ax1.twinx()
+    ax2.hist(area_ratios, bins=bins, alpha=0.99, color='b', zorder=1, label='Frequency')
+    ax2.set_ylabel('Frequency', color='b')
+    ax2.tick_params(axis='y', labelcolor='b')
+
+    bin_width = bins[1] - bins[0]
+    total_count = len(area_ratios)
+    ax2.plot(
+        x,
+        stats.norm.pdf(x, mean, std) * bin_width * total_count, 
+        'g-', lw=3, zorder=10, 
+        label=f'Normal (μ={mean:.4f}, σ={std:.4f})'
+    )
+    ax1.axvline(x=1.00, color='r', linestyle='--', lw=1.5, zorder=11, label='Theoretical Area Ratio = 1.00')
+    plt.savefig("evaluation_results/area_ratio_distribution.png")
+    
+    plt.savefig("evaluation_results/area_ratio_distribution.png")
